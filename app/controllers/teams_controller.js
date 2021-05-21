ebe1d3946c893309b64f835bfcea8a5cd0ba76b1
mizuki-tf <mi_matsui@uluru.jp>
2021-05-21 11:20:14 +0900

    フォーマット修正

--- app/controllers/teams_controller.js
+++ app/controllers/teams_controller.js
@@ -1,11 +1,10 @@
 const { ValidationError } = require('sequelize');
 const Controller = require('./controller');
 const models = require('../models');
-const moment = require('moment-timezone');
 
 class TeamsController extends Controller {
 
-  // GET /creat    
+  // GET /creat
   async create(req, res) {
     res.render('teams/create');
   }
@@ -25,16 +24,13 @@ class TeamsController extends Controller {
       } else {
         throw err;
       }
-    } 
+    }
   }
 
   // GET /:id
   async show(req, res) {
     const team = await this._team(req);
     const tasks = await team.getTeamTask({ order : [['id', 'ASC']] });
-    await tasks.forEach((task) => {
-      task.formattedCreatedAt = moment(task.updatedAt).tz('Asia/Tokyo').format('YYYY/MM/DD HH:mm');
-    });
     res.render('teams/show', { team, tasks });
   }
 
@@ -64,17 +60,13 @@ class TeamsController extends Controller {
   }
 
   async _team(req) {
-<<<<<<< HEAD
     //console.log(req.params.team)
-=======
-    console.log(req.params.team);
->>>>>>> master
     const team = await models.Team.findByPk(req.params.team);
     if (!team) {
       throw new Error('User not find');
     }
     return team;
-    
+
   }
   async _task(req) {
     //console.log(req.params.task)
