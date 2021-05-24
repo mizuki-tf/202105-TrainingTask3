8a61541f719fc94c85284e3039cb0bb7cc9d0a28
mizuki-tf <mi_matsui@uluru.jp>
2021-05-24 14:43:45 +0900

    途中経過

--- app/controllers/top_controller.js
+++ app/controllers/top_controller.js
@@ -6,9 +6,10 @@ class TopController extends Controller {
   //GET /
   async top(req, res) {
     const user = await models.User.findByPk(req.user.id);
-    const teams = await user.getOwnTeams();
+    const teams = await user.getMembers({ include: 'teamInfo' });
+    console.log(JSON.stringify(teams));
     //const assignTasks = await models.Tasks.findAll({ where: { assigneeId: req.user.id } });
-    res.render('index', teams);
+    res.render('index', { teams: teams ,user: user } );
   }
 
 }
