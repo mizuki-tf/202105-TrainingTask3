35ee9026532004a4969623b36a7a33e7f9b8ecb1
mizuki-tf <mi_matsui@uluru.jp>
2021-05-24 19:19:33 +0900

    manager権限を持つメンバーの作成ロジックを追加しよう完成

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const { Route } = require('../lib/route');
 const forceLogin = require('../app/middlewares/force_login');
 const forceAdmin = require('../app/middlewares/force_admin');
+const managebleTeam = require('../app/middlewares/manageble_team');
 
 const route = new Route();
 
@@ -20,8 +21,8 @@ adminRoute.resource('/users', 'admin/users_controller');
 // managerのURL階層の作成
 route.resource('teams', { controller: 'teams_controller', only: [ 'create', 'store' ] });
 
-route.resource('manager/teams', forceLogin, { controller: 'manager/teams_controller', only: [ 'show', 'edit', 'update' ] });
-const managerTeamRoute = route.sub('/manager/teams/:team', forceLogin);
+route.resource('manager/teams', forceLogin, managebleTeam, { controller: 'manager/teams_controller', only: [ 'show', 'edit', 'update' ] });
+const managerTeamRoute = route.sub('/manager/teams/:team', forceLogin, managebleTeam);
 managerTeamRoute.resource('/tasks', { controller: 'manager/tasks_controller', only: [ 'create', 'store', 'edit', 'update' ] });
 managerTeamRoute.resource('/members', { controller: 'manager/members_controller', only: [ 'index', 'store' ] });
 
